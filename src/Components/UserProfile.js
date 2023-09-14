f20557d8df0a2eb992b2475b9dd29648f0761fc6
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2023-09-14 09:38:08 +0530

    add

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { AppSate } from '../Context/AppProvaider'
 import { useNavigate, useParams } from 'react-router-dom'
 import {BiMinusBack} from "react-icons/bi"
